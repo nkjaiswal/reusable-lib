0bc7c4ddc3378880bfaac017d676672757e73221
Nishant Kumar <niskumn@amazon.com>
2020-06-13 06:39:44 -0700

    Adding mail template

--- mailer/mailer_test.js
+++ mailer/mailer_test.js
@@ -2,20 +2,12 @@ var mailer = require("./mailer.js");
 
 var param = [
     {
-        field: "user_name",
-        value: "Nishant"
-    },
-    {
-        field: "sender",
-        value: "Admin"
-    },
-    {
-        field: "sender_email",
-        value: "admin@antifake.biz"
+        field: "singup_complete_link",
+        value: "http://app.severityapp.com"
     }
 ];
 
-mailer.send_mail("antifake.biz", "signup_successfull", "nishant.soft04@gmail.com", "Test Mail", param, function(err, info){
+mailer.send_mail("severityapp", "complete", "nishant.yvr@gmail.com", "Signup", param, function(err, info){
     console.log(err);
     console.log(info);
 })
